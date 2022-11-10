e80c22f1d539a931fc17a57deb3a7355a1bc1c1b
dylmcgold <dmcgoldr@lion.lmu.edu>
2022-11-09 17:06:28 -0800

    Updating work

--- musicmate-reactapp/src/App.js
+++ musicmate-reactapp/src/App.js
@@ -9,7 +9,11 @@ url after redirect login
 http://localhost:3000/#access_token=BQCI5nH4zRAWJtOXGmLMnZtgIWoScWSwSYz_eFgolhJWLk8ySVeAyRcYXmhTGpjmBfVZflUz03EghfZgIXGE7gooqqFNEafSmuTLlOXoz9I3Hhg-Bnr-dGqTnLeYHkvv3gAtX3ID8fTczNOTODYaiNR6vWG8JbvLPGg86g7YDMIIpYzHporNw4hMOuDn9LsFGw&token_type=Bearer&expires_in=3600
 
 */
-const CLIENT_ID = "59759643100f409a9ba8dae7ddbb6a19";
+// var request = require("request"); // "Request" library
+
+var client_id = "1d749561a8d143a996cf153f2f3ed2b2"; // Your client id
+var client_secret = "d13cbcf49c4147c9a98f2b2a62347912"; // Your secret
+// document.querySelector("#search").addEventListener("click", getArtist);
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
 const SPACE_DELIMITER = "%20";
@@ -45,7 +49,7 @@ const WebApp = () => {
     }
   });
   const handleLogin = () => {
-    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${client_id}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
     // "${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_url=${REDIRECT_URL_AFTER_LOGIN}&response_type=token&show_dialog=true";
   };
   return (
@@ -56,6 +60,114 @@ const WebApp = () => {
   );
 };
 export default WebApp;
+
+const getToken = async () => {
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  console.log(data.access_token);
+  return data.access_token;
+};
+getToken();
+
+const getArtist = async (token) => {
+  const name = document.querySelector("#artistName").value;
+  const result = await fetch(
+    `https://api.spotify.com/v1/search?q=${name}&type=artist`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  console.log(data);
+  return data;
+};
+const token = await getToken();
+getArtist(token);
+
+// var authOptions = {
+//   url: "https://accounts.spotify.com/api/token",
+//   headers: {
+//     Authorization:
+//       "Basic " +
+//       new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+//   },
+//   form: {
+//     grant_type: "client_credentials",
+//   },
+//   json: true,
+// };
+// function getArtist() {
+//   const name = document.querySelector("#artistName").value;
+//   request.post(authOptions, function (error, response, body) {
+//     if (!error && response.statusCode === 200) {
+//       // use the access token to access the Spotify Web API
+//       var token = body.access_token;
+//       var options = {
+//         url: `https://api.spotify.com/v1/search?q=${name}&type=artist`,
+//         headers: {
+//           Authorization: "Bearer " + token,
+//         },
+//         json: true,
+//       };
+//       request.get(options, function (error, response, body) {
+//         console.log(body);
+//         // photo = body.artists.items[0].images[2];
+//         document.querySelector(".artistBox").innerHTML = "";
+//         for (let i = 0; i < body.artists.items.length; i++) {
+//           // console.log(i + " " + body.artists.items[i].name);
+//           if (body.artists.items[i].images.length !== 0) {
+//             document.querySelector(".artistBox").innerHTML += `
+//         <div>
+//         <img src="${body.artists.items[i].images[2].url}" alt=${body.artists.items[0].name} />
+//       </div>
+//       <div class="artistInfo">
+//         <h1>${body.artists.items[i].name}</h1>
+//         <p>${body.artists.items[i].genres}</p>
+//       </div>
+//         `;
+//           } else {
+//             document.querySelector(".artistBox").innerHTML += `
+//         <div>
+//         <img src="" alt=${body.artists.items[0].name} />
+//       </div>
+//       <div class="artistInfo">
+//         <h1>${body.artists.items[i].name}</h1>
+//         <p>${body.artists.items[i].genres}</p>
+//       </div>
+//         `;
+//           }
+//         }
+//       });
+//     }
+//   });
+// }
+// request.post(authOptions, function (error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//     // use the access token to access the Spotify Web API
+//     var token = body.access_token;
+//     var options = {
+//       url: `https://api.spotify.com/v1/me`,
+//       headers: {
+//         Authorization: "Bearer " + token,
+//       },
+//       json: true,
+//     };
+//     request.get(options, function (error, response, body) {
+//       console.log(body);
+//     });
+//   }
+// });
 
 // function App() {
 //   return (
