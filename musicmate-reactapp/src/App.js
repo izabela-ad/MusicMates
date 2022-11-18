c301d23749d24ecd89728c76db25157d09373491
dylmcgold <dmcgoldr@lion.lmu.edu>
2022-11-17 17:18:30 -0800

    adding search for react app

--- musicmate-reactapp/src/App.js
+++ musicmate-reactapp/src/App.js
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 import "./App.css";
 //newly added
 import React, { useEffect } from "react";
+import { Buffer } from "buffer";
 //import "./webapp.css";
 
 /*
@@ -12,10 +13,10 @@ http://localhost:3000/#access_token=BQCI5nH4zRAWJtOXGmLMnZtgIWoScWSwSYz_eFgolhJW
 
 */
 // var request = require("request"); // "Request" library
-const CLIENT_ID = "59759643100f409a9ba8dae7ddbb6a19";
-var client_id = ""; // Your client id
-var client_secret = ""; // Your secret
-// document.querySelector("#search").addEventListener("click", getArtist);
+// const CLIENT_ID = "59759643100f409a9ba8dae7ddbb6a19";
+var client_secret = "4dac1536390440628fc6c8cb9057c1e8"; // Your secret
+var CLIENT_ID = "1d749561a8d143a996cf153f2f3ed2b2"; // Your client id
+
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/";
 const SPACE_DELIMITER = "%20";
@@ -58,44 +59,166 @@ const WebApp = () => {
     <div className="container">
       {/* <h1>MusicMate</h1> */}
 
-      <img src={require("./musicmate.jpg")} />
+      {/* <img src={require("./musicmate.jpg")} /> */}
       <button onClick={handleLogin}>Login into Spotify</button>
     </div>
   );
 };
 export default WebApp;
+// const APIController = (function () {
+const getToken = async () => {
+  // var responseClone; // 1
+  // fetch("https://accounts.spotify.com/api/token", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     Authorization:
+  //       "Basic " +
+  //       new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+  //   },
+  //   body: "grant_type=client_credentials",
+  // })
+  //   .then(function (response) {
+  //     responseClone = response.clone(); // 2
+  //     console.log(response.json());
+  //     return response.json();
+  //   })
+  //   .then(
+  //     function (data) {
+  //       // Do something with data
+  //     },
+  //     function (rejectionReason) {
+  //       // 3
+  //       console.log(
+  //         "Error parsing JSON from response:",
+  //         rejectionReason,
+  //         responseClone
+  //       ); // 4
+  //       responseClone
+  //         .text() // 5
+  //         .then(function (bodyText) {
+  //           console.log(
+  //             "Received the following instead of valid JSON:",
+  //             bodyText
+  //           ); // 6
+  //         });
+  //     }
+  //   );
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(CLIENT_ID + ":" + client_secret).toString("base64"),
+    },
+    body: "grant_type=client_credentials",
+  });
+  const data = await result.json();
+  console.log(data.access_token);
+  return data.access_token;
+};
 
-// const getToken = async () => {
-//   const result = await fetch("https://accounts.spotify.com/api/token", {
-//     method: "POST",
-//     headers: {
-//       Authorization:
-//         "Basic " +
-//         new Buffer.from(client_id + ":" + client_secret).toString("base64"),
-//     },
-//     body: "grant_type=client_credentials",
-//   });
+const getArtist = async (token) => {
+  const name = document.querySelector("#artistName").value;
+  const result = await fetch(
+    `https://api.spotify.com/v1/search?q=${name}&type=artist`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+  if (result.ok && result.status === 200) {
+    const data = await result.json();
+    console.log(data);
+    document.querySelector(".artistBox").innerHTML = "";
+    for (let i = 0; i < data.artists.items.length; i++) {
+      // console.log(i + " " + data.artists.items[i].name);
+      if (data.artists.items[i].images.length !== 0) {
+        document.querySelector(".artistBox").innerHTML += `
+          <div>
+          <img src="${data.artists.items[i].images[2].url}" alt=${data.artists.items[0].name} />
+        </div>
+        <div class="artistInfo">
+          <h1>${data.artists.items[i].name}</h1>
+          <p>${data.artists.items[i].genres}</p>
+        </div>
+          `;
+      } else {
+        document.querySelector(".artistBox").innerHTML += `
+          <div>
+          <img src="" alt=${data.artists.items[0].name} />
+        </div>
+        <div class="artistInfo">
+          <h1>${data.artists.items[i].name}</h1>
+          <p>${data.artists.items[i].genres}</p>
+        </div>
+          `;
+      }
+    }
+  }
 
-//   const data = await result.json();
-//   console.log(data.access_token);
-//   return data.access_token;
-// };
-// getToken();
+  // console.log(data);
+  // return data;
+  //   request.post(authOptions, function (error, response, body) {
+  //     if (!error && response.statusCode === 200) {
+  //       // use the access token to access the Spotify Web API
+  //       var token = body.access_token;
+  //       var options = {
+  //         url: `https://api.spotify.com/v1/search?q=${name}&type=artist`,
+  //         headers: {
+  //           Authorization: "Bearer " + token,
+  //         },
+  //         json: true,
+  //       };
+  //       request.get(options, function (error, response, body) {
+  //         console.log(body);
+  //         // photo = body.artists.items[0].images[2];
+  //         document.querySelector(".artistBox").innerHTML = "";
+  //         for (let i = 0; i < body.artists.items.length; i++) {
+  //           // console.log(i + " " + body.artists.items[i].name);
+  //           if (body.artists.items[i].images.length !== 0) {
+  //             document.querySelector(".artistBox").innerHTML += `
+  //         <div>
+  //         <img src="${body.artists.items[i].images[2].url}" alt=${body.artists.items[0].name} />
+  //       </div>
+  //       <div class="artistInfo">
+  //         <h1>${body.artists.items[i].name}</h1>
+  //         <p>${body.artists.items[i].genres}</p>
+  //       </div>
+  //         `;
+  //           } else {
+  //             document.querySelector(".artistBox").innerHTML += `
+  //         <div>
+  //         <img src="" alt=${body.artists.items[0].name} />
+  //       </div>
+  //       <div class="artistInfo">
+  //         <h1>${body.artists.items[i].name}</h1>
+  //         <p>${body.artists.items[i].genres}</p>
+  //       </div>
+  //         `;
+  //           }
+  //         }
+  //       });
+  //     }
+  //   });
+  // };
+};
+// getArtist(getToken());
+// document.querySelector("#search").addEventListener("click", getArtist);
 
-// const getArtist = async (token) => {
-//   const name = document.querySelector("#artistName").value;
-//   const result = await fetch(
-//     `https://api.spotify.com/v1/search?q=${name}&type=artist`,
-//     {
-//       method: "GET",
-//       headers: { Authorization: "Bearer " + token },
-//     }
-//   );
+const loadData = async () => {
+  //get the token
+  const token = await getToken();
+  //get the genres
+  const artist = await getArtist(token);
+  //populate our genres select element
+  // genres.forEach((element) => UICtrl.createGenre(element.name, element.id));
+};
+document.querySelector("#search").addEventListener("click", loadData);
 
-//   const data = await result.json();
-//   console.log(data);
-//   return data;
-// };
+// })();
+// APIController.init();
 // const token = await getToken();
 // getArtist(token);
 
@@ -173,23 +296,23 @@ export default WebApp;
 //   }
 // });
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <p>Music Mate</p>
-//         {/* <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         > */}
-//         <button>Sign in to Spotify</button>
-//         {/* </a> */}
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Music Mate</p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > */}
+        <button>Sign in to Spotify</button>
+        {/* </a> */}
+      </header>
+    </div>
+  );
+}
 
 // export default App;
